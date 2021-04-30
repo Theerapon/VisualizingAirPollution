16a9636dfe36aab4bf0e9a440c6d2a41818bb315
Theerapon <Theerapon.nanoi.185@gmail.com>
2021-04-30 23:00:25 +0700

    initial Spring Boot with Kotlin

---
+++ src/main/kotlin/com/gis/spatial/visualizingAirPollution/VisualizingAirPollutionApplication.kt
@@ -0,0 +1,11 @@
+package com.gis.spatial.visualizingAirPollution
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class VisualizingAirPollutionApplication
+
+fun main(args: Array<String>) {
+	runApplication<VisualizingAirPollutionApplication>(*args)
+}
---
+++ src/test/kotlin/com/gis/spatial/visualizingAirPollution/VisualizingAirPollutionApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.gis.spatial.visualizingAirPollution
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class VisualizingAirPollutionApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
